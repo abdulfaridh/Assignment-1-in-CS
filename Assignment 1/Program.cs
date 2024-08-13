549bb79b711601e733fd3bfa9db6327dadaebca3
farid <farid@LAPTOP-SPTVSQVM>
2024-08-13 19:55:18 +0530

    Add project files.

---
+++ Assignment 1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Assignmet1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter a integer input ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"your int is : {n}");
+    }
+}
